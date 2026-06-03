8236dba4006e28b912ae600701c6cae733a8b264
Muthu kumaaran <muthuc9pro2@gmail.com>
2026-06-03 12:54:10 +0530

    update

---
+++ pythonSet.py
@@ -0,0 +1,26 @@
+# set is unordered collection of items its mutable and it just eliminate the duplicates it is defined by {}
+
+a = {1, 2, 3, 4, 5}
+print(a)  # Sets are unordered, so indexing is not possible
+
+# dictionaries and set are defined by using {} but the difference is that dictionaries have key and value pairs 
+
+# we can also change the set to list and vice versa by set() and list() respectively
+
+uber_city = ["chennai", "bangalore", "bangalore", "mumbai", "delhi", "kolkata"]
+unique_cites = set(uber_city)
+print(unique_cites)
+
+uber_city1 = {"chennai", "bangalore", "bangalore", "mumbai", "delhi", "kolkata"}
+uber_city2 = {"seattle", "san francisco", "new york", "chicago", "boston", "chennai", "bangalore"}
+
+print(uber_city1.union(uber_city2)) 
+print(uber_city1.intersection(uber_city2))
+print(uber_city1.difference(uber_city2))
+
+# set can be used to perform set operations like union, intersection and difference etc 
+# set is mutable but it can only add or remove but not repplace or update the existing element in the set
+
+
+
+
--- tuples.py
+++ tuples.py
@@ -1,1 +1,36 @@
-trip_summary=("uberGo", "chennai", "airport", 450.00, "completed") 
+trip_summary=("uberGo", "chennai", "airport", 450.00, "completed")
+print(trip_summary)
+
+print(trip_summary[1])
+
+for item in trip_summary:
+    print(item)
+
+# tuples is just like List but it is immutable and it is defined by using () instead of [] and it is used to store multiple items in a single variable and it is ordered and it allows duplicate values and it is indexed and it is unchangeable and it is faster than list because of its immutability       
+
+# list is dynamic but tuple is static
+
+
+# dictionaries 
+
+student = {
+    "name": "Muthu",
+    "age": 21,
+    "city": "Chennai"
+}
+
+print(student["name"])
+
+# dictionaries is just like object in js 
+
+'''
+ js object example:
+
+const student = {
+    name: "Muthu",
+    age: 22,
+    city: "Chennai"
+}
+
+console.log(student.name);
+'''
