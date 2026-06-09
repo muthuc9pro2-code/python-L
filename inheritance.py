2a0bb9122a4e7420011a5e6e778868b56e55ed10
Muthu kumaaran <muthuc9pro2@gmail.com>
2026-06-09 19:45:31 +0530

    update

--- inheritance.py
+++ inheritance.py
@@ -58,6 +58,92 @@ class exportFile():
 
 # this above code is only for export to other file so check the other file regarding this above exportFile 
 
+class grandDad():
+    def cycle(self):
+        print("silver")
+
+class dad(grandDad): 
+    def house(self):
+        print("red")
+
+class son(dad):
+    def factory(self):
+        print("white")
+
+    def house(self):
+        print("yellow")
+
+obj = son()
+obj.factory()
+obj.house()
+obj.cycle()
+
+# this is multilevel inheritance
+
+class dad(): #parent
+    def house(self):
+        print("red")
+
+class son1(dad): #child1
+    def factory(self):
+        print("white")
+
+    def house(self):
+        print("yellow")
+
+class son2(dad): #child1
+    def market(self):
+        print("fruits")
+
+    def house(self):
+        print("green")
+
+
+
+obj1 = son1()
+obj1.factory()
+obj1.house()
+
+obj2 = son2()
+obj2.market()
+obj2.house()
+
+# okay the above code is called heirarchy inherritance ,a parent can have multiple child but each child should have seperate object 
+# there is no relationship between childrens they just connected with parent class
+
+#likewise a child can have more than one parent class by using , in () this is called multiple inherritance
+
+class dad(): #parent
+    def house(self):
+        print("red")
+
+class mom(): #parent
+    def shop(self):
+        print("white")
+
+    def house(self):
+        print("yellow")
+
+class son(dad,mom): #child
+    def market(self):
+        print("fruits")
+
+    def house(self):
+        print("blue")
+
+    def shop(self):
+        print("green")
+
+obj = son()
+obj.house()
+obj.shop()
+
+# there is another one too called hybrid which is nothing but using all things together like multilevel, multiple, single, heirarchy inherritance combining this things is called hybrid inherritance
+
+
+
+
+
 
 
 
